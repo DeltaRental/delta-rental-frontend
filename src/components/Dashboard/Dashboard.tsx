53fff014dd660bb1c5715468ab66b370f823b05c
rdvngrsy <ridvangursoy@hotmail.com>
2024-02-02 13:05:35 +0300

    Stripe ile ödeme sayfası eklendi.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ import CarDetail from "../../pages/Car/CarDetail";
 import CarAdd from "../../pages/Car/CarAdd";
 import HomePage from "../../pages/HomePage";
 import CarAvailability from "../../pages/Car/CarAvailability";
+import PaymentPage from "../../pages/PaymentPage/PaymentPage";
 
 const Dashboard = () => {
   return (
@@ -17,6 +18,7 @@ const Dashboard = () => {
               <Route path="/" element={<HomePage />}></Route>
               <Route path="/cars/getAll" element={<CarList />}></Route>
               <Route path="/cars/getCarAvailability" element={<CarAvailability />}></Route>
+              <Route path="/payment" element={<PaymentPage />}></Route>
               <Route path="/cars/:id" element={<CarDetail/>}></Route>
               <Route path="/car/add" element={<CarAdd/>}></Route>
               <Route path="*" element={<div>Not found</div>}></Route>
---
+++ src/pages/PaymentPage/PaymentPage.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../../components/PaymentCheckoutForm/CheckoutForm";
+import "../PaymentPage/PaymentPage.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  "pk_test_51OecbiDJwyPdNC8AxXpr0WRdPjv8c4gBc22SV3VcfHgXc4MRLeFNw4j0lsucMdUZ14JpdH0J0r9FpoPIFLCModpK00GqqhNjgY"
+);
+
+interface PaymentPageProps {}
+
+const PaymentPage: React.FC<PaymentPageProps> = () => {
+  const [clientSecret, setClientSecret] = useState<string>("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8080/api/payments/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe" as const,
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+      <div className="h-screen flex justify-center items-center">
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        )}
+      </div>
+  );
+};
+
+export default PaymentPage;
